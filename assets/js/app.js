f5e0830050c660179ef3ba3c179dd8fa2392227f
LeidyRosmery <rosm3rita@hotmail.com>
2017-07-01 20:50:04 -0500

    portafolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,12 @@ var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
 setTimeout(function() {
   $cont.classList.remove('s--inactive');
 }, 200);
+
+$("#turn-off").on("change", function(){
+  if ($(this).prop( "checked" )) {
+    $(".background-div").addClass("background-attach")
+  }
+  else {
+     $(".background-div").removeClass("background-attach")
+  }
+})

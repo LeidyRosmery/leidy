424f69dd863c82bc50f9a712df99673fa3a0124a
LeidyRosmery <rosm3rita@hotmail.com>
2017-07-02 02:27:55 -0500

    portafolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,28 @@
 var $cont = document.querySelector('.cont');
-var $elsArr = [].slice.call(document.querySelectorAll('.el'));
-var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
 
 setTimeout(function() {
   $cont.classList.remove('s--inactive');
 }, 200);
+
+var lastScrollTop = 0;
+$( window ).scroll(function() {
+   console.log("entra");
+  var pantalla= $(window).width();
+  var currentScroll=window.pageYOffset || document.documentElement.scrollTop;
+
+
+  if (currentScroll < 340){
+    $('#portfolio').addClass('top-line');
+    //$('#portfolio').removeClass('top-line-hidden-black ');
+  }
+  if (currentScroll < 440){
+    $('#about').addClass('top-line');
+    //$('#portfolio').removeClass('top-line-hidden-black ');
+  }
+  if (currentScroll < 540){
+    $('#contact').addClass('top-line');
+    //$('#portfolio').removeClass('top-line-hidden-black ');
+  }
+
+    lastScrollTop = currentScroll;
+});
